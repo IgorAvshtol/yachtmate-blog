32516e715d9867f95f57bd7a0d084fbfbdc678bd
IharAshtol <iharrwe@gmail.com>
2022-09-01 16:03:27 +0300

    fix: add useEffect in Layout;

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,28 @@
 import { Flex } from '@chakra-ui/react';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 import { Header } from './Header/Header';
 import { TabsBlock } from './TabsBlock';
 import { Footer } from './Footer/Footer';
+import { useAppContext } from 'hooks/useAppContext';
+import { getLangFromLocalStorage } from 'services/localStorage';
 
 interface ILayout {
   children: ReactNode;
 }
 
 export const Layout = ({ children }: ILayout) => {
+  const { setCurrentLanguage, setCurrentArticleTab } = useAppContext();
+
+  useEffect(() => {
+    setCurrentArticleTab('');
+  }, [setCurrentArticleTab]);
+
+  useEffect(() => {
+    const currentLang = getLangFromLocalStorage();
+    setCurrentLanguage(currentLang);
+  }, [setCurrentLanguage]);
+
   return (
       <Flex minH='100vh' w='100%' h='100%' alignItems='center' flexDirection='column'>
         <Header/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,8 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
 
 import { Main } from 'components/Main/Main';
-import { getLangFromLocalStorage } from 'services/localStorage';
-import { useAppContext } from 'hooks/useAppContext';
 
 const Home = (): JSX.Element => {
-  const { setCurrentLanguage, setCurrentArticleTab } = useAppContext();
-
-  useEffect(() => {
-    setCurrentArticleTab('');
-  }, [setCurrentArticleTab]);
-
-  useEffect(() => {
-    const currentLang = getLangFromLocalStorage();
-    setCurrentLanguage(currentLang);
-  }, [setCurrentLanguage]);
-
   return (
       <>
         <Head>
