056cf14b089c2d3d7560fafa8f1d88ac2b7d27f3
IharAshtol <iharrwe@gmail.com>
2022-09-15 22:02:03 +0300

    fix: delete unused import;

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -4,21 +4,17 @@ import { useRouter } from 'next/router';
 import { Button, Menu as MainBlock, MenuButton, MenuItem, MenuList, } from '@chakra-ui/react';
 
 import menuLogo from 'public/menu.png';
-import { useAppDispatch } from 'store/store';
 
 export const Menu = (): JSX.Element => {
-  const dispatch = useAppDispatch();
   const router = useRouter();
 
   const handleLanguageToggle = async (e: React.MouseEvent<HTMLButtonElement>) => {
     switch (e.currentTarget.textContent) {
       case 'en':
         await router.push(`${router.asPath}`, `${router.asPath}`, { locale: 'en' });
-        dispatch(changeLanguage('en'));
         break;
       case 'ru':
         await router.push(`${router.asPath}`, `${router.asPath}`, { locale: 'ru' });
-        dispatch(changeLanguage('ru'));
         break;
     }
   };
