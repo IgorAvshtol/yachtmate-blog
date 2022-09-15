4c51d93a0c26d953194f045179ef37a6246bf85e
IharAshtol <iharrwe@gmail.com>
2022-09-15 23:22:26 +0300

    fix: change ui for same articles;

--- src/components/SameArticles/SameArticles.tsx
+++ src/components/SameArticles/SameArticles.tsx
@@ -13,13 +13,14 @@ export const SameArticles = (): JSX.Element => {
   const t = router.locale === 'en' ? eng : rus;
 
   return (
-      <Flex direction='column' alignItems='center' w='100%' mt={{ md: '104px', sm: '52px' }} mb='80px'>
-        <Text w='80%' fontWeight='600' fontSize='26px' lineHeight='110%' letterSpacing='0.3px' color='#001240'
+      <Flex direction='column' alignItems='center' maxW='1200px' w='100%' mt={{ md: '104px', sm: '52px' }} mb='80px'>
+        <Text w={{ lg: '90%', md: '80%', sm: '90%' }} fontWeight='600' fontSize='26px' lineHeight='110%'
+              letterSpacing='0.3px' color='#001240'
               textAlign={{ md: 'start', sm: 'center' }}>
           {t.same_way.title}
         </Text>
-        <Flex direction='column' w={{ md: '80%', sm: '90%' }}>
-          <Flex justifyContent='space-between' m='20px -20px 0' overflowX={{ md: 'hidden', sm: 'scroll' }}
+        <Flex direction='column' w={{ lg: '90%', md: '80%', sm: '90%' }}>
+          <Flex justifyContent='space-between' m='20px -20px 0' overflowX={{ xl: 'hidden', lg: 'scroll', sm: 'scroll' }}
                 whiteSpace={{ md: 'normal', sm: 'nowrap' }}>
             {sortSameArticles(articles, Number(currentArticleId)).map(article => <SameArticle key={article.id}
                                                                                               slug={article.attributes.slug}
