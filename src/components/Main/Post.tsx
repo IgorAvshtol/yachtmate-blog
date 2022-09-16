867417d1487223d63cec247788abcf16da1bb8ff
IharAshtol <iharrwe@gmail.com>
2022-09-16 15:22:21 +0300

    feat: add zoom effect for hover on some post;

--- src/components/Main/Post.tsx
+++ src/components/Main/Post.tsx
@@ -16,17 +16,20 @@ export const Post = ({ image, title, date, slug, lang }: IPost): JSX.Element => 
 
   return (
       <GridItem h='max-content' p='20px' bg='white' borderRadius='12px' _even={{ md: { mt: '30px', sm: '0' } }}
-                _odd={{ md: { mt: '-200px', sm: '0' } }}>
-        <Box borderRadius='8px' overflow='hidden'>
-          <Image src={image} objectFit='cover' layout='responsive' width='400px' height='226px' alt='postCover'/>
-        </Box>
-        <Text mt='20px' mb='12px' fontSize='14px' letterSpacing='0.5px' opacity='0.5'>
-          {correctDate}
-        </Text>
+                _odd={{ md: { mt: '-200px', sm: '0' } }} transition='all .25s ease'
+                _hover={{ transform: 'scale(1.1)' }}>
         <Link href={`${lang}/blog/${slug}`}>
-          <Text as='h2' fontSize={{ md: '26px', sm: '20px' }}>
-            {title}
+          <Box borderRadius='8px' overflow='hidden'>
+            <Image src={image} objectFit='cover' layout='responsive' width='400px' height='226px' alt='postCover'/>
+          </Box>
+          <Text mt='20px' mb='12px' fontSize='14px' letterSpacing='0.5px' opacity='0.5'>
+            {correctDate}
           </Text>
+          <Link href={`${lang}/blog/${slug}`}>
+            <Text as='h2' fontSize={{ md: '26px', sm: '20px' }}>
+              {title}
+            </Text>
+          </Link>
         </Link>
       </GridItem>
   );
--- src/components/SameArticles/SameArticles.tsx
+++ src/components/SameArticles/SameArticles.tsx
@@ -15,18 +15,18 @@ export const SameArticles = (): JSX.Element => {
   return (
       <Flex direction='column' alignItems='center' maxW='1200px' w='100%' mt={{ md: '104px', sm: '52px' }} mb='80px'>
         <Text w={{ lg: '90%', md: '80%', sm: '90%' }} fontWeight='600' fontSize='26px' lineHeight='110%'
-              letterSpacing='0.3px' color='#001240'
-              textAlign={{ md: 'start', sm: 'center' }}>
+              letterSpacing='0.3px' color='#001240' textAlign={{ md: 'start', sm: 'center' }}>
           {t.same_way.title}
         </Text>
         <Flex direction='column' w={{ lg: '90%', md: '80%', sm: '90%' }}>
           <Flex justifyContent='space-between' m='20px -20px 0' overflowX={{ xl: 'hidden', lg: 'scroll', sm: 'scroll' }}
                 whiteSpace={{ md: 'normal', sm: 'nowrap' }}>
-            {sortSameArticles(articles, Number(currentArticleId)).map(article => <SameArticle key={article.id}
-                                                                                              slug={article.attributes.slug}
-                                                                                              title={article.attributes.main_title}
-                                                                                              date={article.attributes.createdAt}
-                                                                                              image={article.attributes.main_image.url}/>)
+            {
+              sortSameArticles(articles, Number(currentArticleId)).map(article => <SameArticle key={article.id}
+                                                                                               slug={article.attributes.slug}
+                                                                                               title={article.attributes.main_title}
+                                                                                               date={article.attributes.createdAt}
+                                                                                               image={article.attributes.main_image.url}/>)
             }
           </Flex>
         </Flex>
