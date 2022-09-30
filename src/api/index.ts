afb4b28c9baa63481bfaf455927373318adffb17
IharAshtol <iharrwe@gmail.com>
2022-09-30 12:49:38 +0300

    fix: instance for auth;

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,6 +10,13 @@ export const instance = axios.create({
 export const instanceForAuth = axios.create({
   withCredentials: true,
   baseURL: process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
+    'Access-Control-Allow-Headers': 'Authorization, X-Custom-Header',
+    'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
+    mode: 'cors',
+  },
 });
 
 export const HttpConfig = {
