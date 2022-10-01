44088b0f61ccf209a4444e07c4e46bb0522eec3e
IharAshtol <iharrwe@gmail.com>
2022-10-01 13:04:53 +0300

    fix: header for axios;

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,8 +12,7 @@ export const instanceForAuth = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH,
   headers: {
     'Content-Type': 'application/json;charset=utf-8',
-    // 'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
-    'Access-Control-Allow-Origin': 'https://blog.test.yachtmate.club/',
+    'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Headers': 'Authorization, X-Custom-Header',
     'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
