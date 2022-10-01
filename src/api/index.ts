8a08775dc273595b64f6613273c7ef9f32fbb8e9
IharAshtol <iharrwe@gmail.com>
2022-10-01 12:35:43 +0300

    fix: header for axios;

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,9 +11,9 @@ export const instanceForAuth = axios.create({
   withCredentials: true,
   baseURL: process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH,
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json;charset=utf-8',
     // 'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://blog.test.yachtmate.club/',
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Headers': 'Authorization, X-Custom-Header',
     'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
@@ -21,59 +21,59 @@ export const instanceForAuth = axios.create({
   },
 });
 
-// export const HttpConfig = {
-//   withCredentials: true,
-//   headers: {
-//     'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
-//     'Access-Control-Allow-Credentials': true,
-//   },
-// };
-//
-// instanceForAuth.interceptors.request.use((config: AxiosRequestConfig) => {
-//   config.withCredentials = true;
-//
-//   config.headers = {
-//     'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
-//     'Access-Control-Allow-Credentials': true,
-//   };
-//
-//   const token = getUserFromLocalStorage();
-//   config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-//
-//
-// instanceForAuth.interceptors.response.use(
-//     (config: AxiosRequestConfig) => {
-//       config.withCredentials = true;
-//
-//       config.headers = {
-//         'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
-//         'Access-Control-Allow-Credentials': true,
-//       };
-//
-//       return config;
-//     },
-//     async (error) => {
-//       const originalRequest = error.config;
-//       if (
-//           error.response.status === 401 &&
-//           error.config &&
-//           !error.config._isRetry
-//       ) {
-//         originalRequest._isRetry = true;
-//         try {
-//           const response = await axios.get(
-//               `${process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string}/user/token/refresh`, HttpConfig
-//           );
-//
-//           setUserFromLocalStorage(response.data.accessToken);
-//           // localStorage.setItem('token', response.data.accessToken);
-//           return instanceForAuth.request(originalRequest);
-//         } catch (e) {
-//           console.log('Non auth');
-//         }
-//       }
-//       throw error;
-//     }
-// );
+export const HttpConfig = {
+  withCredentials: true,
+  headers: {
+    'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
+    'Access-Control-Allow-Credentials': true,
+  },
+};
+
+instanceForAuth.interceptors.request.use((config: AxiosRequestConfig) => {
+  config.withCredentials = true;
+
+  config.headers = {
+    'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
+    'Access-Control-Allow-Credentials': true,
+  };
+
+  const token = getUserFromLocalStorage();
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+
+instanceForAuth.interceptors.response.use(
+    (config: AxiosRequestConfig) => {
+      config.withCredentials = true;
+
+      config.headers = {
+        'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string,
+        'Access-Control-Allow-Credentials': true,
+      };
+
+      return config;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+      if (
+          error.response.status === 401 &&
+          error.config &&
+          !error.config._isRetry
+      ) {
+        originalRequest._isRetry = true;
+        try {
+          const response = await axios.get(
+              `${process.env.NEXT_PUBLIC_BASE_URL_FOR_AUTH as string}/user/token/refresh`, HttpConfig
+          );
+
+          setUserFromLocalStorage(response.data.accessToken);
+          // localStorage.setItem('token', response.data.accessToken);
+          return instanceForAuth.request(originalRequest);
+        } catch (e) {
+          console.log('Non auth');
+        }
+      }
+      throw error;
+    }
+);
