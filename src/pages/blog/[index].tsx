25700f8d6a118041f38b93c0d6264882599cc6cb
IharAshtol <iharrwe@gmail.com>
2022-10-17 18:54:49 +0300

    fix: update useeffect in index.tsx;

--- src/pages/blog/[index].tsx
+++ src/pages/blog/[index].tsx
@@ -21,7 +21,6 @@ const Article = (): JSX.Element => {
   const t = locale === 'en' ? eng : rus;
   const { currentArticle: data, loading } = useAppSelector(state => state.articles);
   const [html, setHtml] = useState<string | null>(null);
-  const [showChild, setShowChild] = useState(false);
 
   useEffect(() => {
     const currentArticleURLData = {
@@ -30,7 +29,6 @@ const Article = (): JSX.Element => {
     };
     currentArticleURLData.slug && dispatch(getCurrentArticle(currentArticleURLData));
     data?.id && dispatch(setOneViewForArticle(data?.id));
-    setShowChild(true);
   }, [data?.id, dispatch, locale, query]);
 
   useEffect(() => {
@@ -46,7 +44,7 @@ const Article = (): JSX.Element => {
     setHtml(fragment.innerHTML);
   }, [data]);
 
-  if (loading === TypeLoadingStatus.IS_PENDING || !showChild) return <ArticlePageWithSkeleton/>;
+  if (loading === TypeLoadingStatus.IS_PENDING) return <ArticlePageWithSkeleton/>;
 
   return (
       <>
