7c43a3ffa1aed725101158a6c2bec9acd8cd55ef
IharAshtol <iharrwe@gmail.com>
2022-11-08 16:31:27 +0300

    fix-yac634/change locale in url in server-sitemap.xml;

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -4,12 +4,11 @@ import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
 import { IResponseArticles } from 'interfaces';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const lang = context?.locale;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/articles?locale=${lang}`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/articles?locale=all`);
   const { data }: IResponseArticles = await response.json();
 
   const fields: ISitemapField[] = data.map(article => ({
-    loc: `${process.env.NEXT_PUBLIC_BASE_MAIN_PAGE_URL}/${lang}/blog/${article.attributes.slug}`,
+    loc: `${process.env.NEXT_PUBLIC_BASE_MAIN_PAGE_URL}/${article.attributes.locale}/blog/${article.attributes.slug}`,
     lastmod: new Date().toISOString()
   }));
 
